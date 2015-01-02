5d46ec00b26fb08b52d2ce1990b966c4980d6a16
jmounts <mountzer@gmail.com>
2015-01-02 09:36:05 -0600

    strike

--- js/main.js
+++ js/main.js
@@ -2,10 +2,17 @@ $(document).ready(function(){
 
 	console.log("working");
 
+//input//
 	$("#input-b").on('click', function(){
 		var input = $('#myInput').val()
-			$('#list').append('<li>' + input + '</li>');
+			$('#list').prepend('<li class="item"><button class="mark">☑</button>' + input + '</li>');
 		$('#myInput').val('');
 	})
+
+//Strike-through//
+	$(document).on('click', '.mark', function(){
+		$(this).closest('li').toggleClass('strike');
+	})
+
 
 });
