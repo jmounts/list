72d789a5667a1e01e696698c4caf6ae6a7823d48
jmounts <mountzer@gmail.com>
2015-01-02 12:00:42 -0600

    alert

--- js/main.js
+++ js/main.js
@@ -5,9 +5,15 @@ $(document).ready(function(){
 //input//
 	$("#input-b").on('click', function(){
 		var input = $('#myInput').val()
+		if($('#myInput').val().trim().length == 0){
+			alert("Forgot Something?");
+		}
+		else	
+		{
 			$('#list').prepend('<li class="item"><button class="mark">☑</button>' + input + '<button class="remove-item">X</button></li>');
 		$('#myInput').val('');
-	})
+		}
+	});
 
 //Strike-through//
 	$(document).on('click', '.mark', function(){
